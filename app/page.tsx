ffbbeceb4f64bb75c66902ee3a2b5650c457ef10
Brian <brianizzo93@gmail.com>
2024-01-13 17:40:03 +0900

    Basic Layout set

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,19 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <div className='grid-cols-2'>
-      <header>
-
+    <div className='h-screen grid grid-cols-3 gap-4 container mx-auto'>
+      <header className='col-span-3'>
+        Text Header
       </header>
 
-        <main>
-
+        <main className='col-span-2 row-start-2'>
+          This is the main area.
         </main>
-        <aside>
-
+        <aside className='col-start-3 row-start-2'>
+          This is the side bar.
         </aside>
 
-      <footer>
-
+      <footer className='row-start-3 col-span-3'>
+        This is the footer.
       </footer>
     </div>
   )
